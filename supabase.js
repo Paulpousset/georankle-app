6cc0ce7e902f90a9e1f4101cbaa9b14412d2a8c3
Pousset Paul <90238839+Paulpousset@users.noreply.github.com>
2026-05-03 15:33:45 +0200

    chore: update Supabase client configuration to use environment variables

--- supabase.js
+++ supabase.js
@@ -2,8 +2,8 @@ import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const supabaseUrl = 'https://exwfggaytrywnfzcqpel.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4d2ZnZ2F5dHJ5d25memNxcGVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NDA5NjAsImV4cCI6MjA5MjUxNjk2MH0.AZkKT-wiJppVpFl3Pz2i_nwHGCSEng7escy6aO_lFOs';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
